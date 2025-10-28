6bcc2f77a6ddae2c0d4a4bcb7e276f60bf17ce01
Raihanjansmaillendra <raihanjansmaillendra.rjs@gmail.com>
2025-10-28 11:31:25 +0700

    Add Navbar and Sidebar components to enhance layout and navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -8,6 +10,8 @@ function App() {
   return (
     <>
       <div>
+        <Navbar />
+        <Sidebar />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
