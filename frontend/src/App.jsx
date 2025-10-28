940cfc8ba2be0ad49c4f5234e913d9b4c336081a
Raihanjansmaillendra <raihanjansmaillendra.rjs@gmail.com>
2025-10-28 15:58:09 +0700

    Implement routing with react-router-dom and restructure App component layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import FaceScan from "./page/FaceScan";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <div>
-        <Navbar />
-        {/* <Sidebar /> */}
-        <FaceScan />
+    <Router>
+      <div className="flex bg-slate-950">
+        <Sidebar />
+
+        <div className="flex-1 flex flex-col">
+          <Navbar />
+          <main className="">
+            <Routes>
+              <Route path="/face-scan" element={<FaceScan />} />
+            </Routes>
+          </main>
+        </div>
       </div>
-    </>
+    </Router>
   );
 }
-
-export default App;
